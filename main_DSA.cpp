cb3170b653b3cc2d9234add665c9d418861fcddd
nojabbar <nojabbar@ada.local.isima.fr>
2024-10-27 14:18:48 +0100

    attak implemented + small changes on the previous code

--- main_DSA.cpp
+++ main_DSA.cpp
@@ -45,19 +45,20 @@ void KeyGen(mpz_t p, mpz_t q, mpz_t g, mpz_t y, mpz_t x) {
     gmp_randclear(r);
 }
 
-void afficher(const char* nom, const mpz_t var) {
+void afficher(const char* nom, mpz_t var) {
     char tab[1024]; 
     mpz_get_str(tab, 10, var);
     std::cout << nom << " = " << tab << std::endl;
 }
 
-void hachage(const std::string& message, mpz_t result) {
+void hachage(std::string& message, mpz_t result, mpz_t q) {
     unsigned char hash[SHA256_DIGEST_LENGTH];
     SHA256(reinterpret_cast<const unsigned char*>(message.c_str()), message.size(), hash);
     mpz_import(result, SHA256_DIGEST_LENGTH, 1, 1, 0, 0, hash);
+    mpz_mod(result, result, q);
 }
 
-void Sign(mpz_t p, mpz_t q, mpz_t g, mpz_t x, mpz_t r, mpz_t s, mpz_t h_m) {
+void Sign(mpz_t p, mpz_t q, mpz_t g, mpz_t x, mpz_t r, mpz_t s, mpz_t h_m, mpz_t k_out) {
     gmp_randstate_t rand_state;
     gmp_randinit_default(rand_state);
     gmp_randseed_ui(rand_state, time(NULL));
@@ -65,28 +66,29 @@ void Sign(mpz_t p, mpz_t q, mpz_t g, mpz_t x, mpz_t r, mpz_t s, mpz_t h_m) {
     mpz_t k, temp, k_inv;
     mpz_inits(k, temp, k_inv, NULL);
 
-    while (1) {
+    while (true) {
         mpz_urandomm(k, rand_state, q);
-        if (mpz_cmp_ui(k, 0) == 0) continue;  
+        if (mpz_cmp_ui(k, 0) == 0) continue;
 
-        mpz_powm(temp, g, k, p);  
-        mpz_mod(r, temp, q);      
-        if (mpz_cmp_ui(r, 0) == 0) continue;  
+        mpz_powm(temp, g, k, p);
+        mpz_mod(r, temp, q);
+        if (mpz_cmp_ui(r, 0) == 0) continue;
 
-        mpz_mul(temp, x, r);      
-        mpz_add(temp, temp, h_m); 
+        mpz_mul(temp, x, r);
+        mpz_add(temp, temp, h_m);
+        mpz_invert(k_inv, k, q);
+        mpz_mul(s, k_inv, temp);
+        mpz_mod(s, s, q);
+        if (mpz_cmp_ui(s, 0) == 0) continue;
 
-        if (mpz_invert(k_inv, k, q) == 0) continue;  
-
-        mpz_mul(s, k_inv, temp); 
-        mpz_mod(s, s, q);        
-        if (mpz_cmp_ui(s, 0) == 0) continue;  
+        mpz_set(k_out, k); 
         break;
     }
 
     mpz_clears(k, temp, k_inv, NULL);
     gmp_randclear(rand_state);
 }
+
 
 void Verify(mpz_t s, mpz_t q, mpz_t r, mpz_t h_m, mpz_t p, mpz_t g, mpz_t y) {
     mpz_t s_inv, w, u1, u2, v, temp1, temp2;
@@ -125,10 +127,29 @@ void Verify(mpz_t s, mpz_t q, mpz_t r, mpz_t h_m, mpz_t p, mpz_t g, mpz_t y) {
     mpz_clears(s_inv, w, u1, u2, v, temp1, temp2, NULL);
 }
 
-int main() {
-    mpz_t p, q, g, y, x, h_m, r, s, h_m_faux;
+void Attaque(mpz_t x, mpz_t s,  mpz_t k, mpz_t h_m, mpz_t r, mpz_t q) {
+    mpz_t temp1, temp2, r_inv;
+    mpz_inits(temp1, temp2, r_inv, NULL);
 
-    mpz_inits(p, q, g, y, x, h_m, r, s, h_m_faux, NULL);
+    mpz_mul(temp1, s, k);
+    mpz_mod(temp1, temp1, q);
+
+    mpz_sub(temp2, temp1, h_m);
+    mpz_mod(temp2, temp2, q);
+
+    mpz_invert(r_inv, r, q);
+
+    mpz_mul(x, temp2, r_inv);
+    mpz_mod(x, x, q);
+
+    mpz_clears(temp1, temp2, r_inv, NULL);
+}
+
+
+int main() {
+    mpz_t p, q, g, y, x, h_m, r, s, h_m_faux, k, x_found;
+
+    mpz_inits(p, q, g, y, x, h_m, r, s, h_m_faux, k, x_found, NULL);
 
     KeyGen(p, q, g, y, x);
     afficher("p", p);
@@ -137,31 +158,41 @@ int main() {
     afficher("y", y);
     afficher("x", x);
 
-    std::string message = "message à signer.";
-    hachage(message, h_m);
+    std::string message = "msg to be signed";
+    hachage(message, h_m, q);
 
-    Sign(p, q, g, x, r, s, h_m);
-    afficher("Signature r", r);
-    afficher("Signature s", s);
+    Sign(p, q, g, x, r, s, h_m, k);
+    afficher("r", r);
+    afficher("s", s);
+    afficher("k", k);
 
-    std::cout << "Test de la signature valide : " << std::endl;
+    std::cout << "test valid signature" << std::endl;
     Verify(s, q, r, h_m, p, g, y);
 
-    std::string faux_message = "Bonjour, ceci est un message modifié.";
-    hachage(faux_message, h_m_faux);
+    /*std::string faux_message = "message modifié.";
+    hachage(faux_message, h_m_faux, q);
     std::cout << " Test avec message modifié : " << std::endl;
     Verify(s, q, r, h_m_faux, p, g, y);
 
     mpz_add_ui(r, r, 1); 
-    std::cout << "r modifié..." << std::endl;
+    std::cout << "r modifié" << std::endl;
     Verify(s, q, r, h_m, p, g, y);
     mpz_sub_ui(r, r, 1); 
 
     mpz_add_ui(s, s, 1);
-    std::cout << "s modifié..." << std::endl;
+    std::cout << "s modifié" << std::endl;
     Verify(s, q, r, h_m, p, g, y);
+    */
+    Attaque(x_found, s, k, h_m, r, q);
+    afficher("x privé", x);
+    afficher("x retrouvé", x_found);
 
-    mpz_clears(p, q, g, y, x, h_m, r, s, h_m_faux, NULL);
+    if (mpz_cmp(x, x_found) == 0) {
+        std::cout << "x found" << std::endl;
+    } else {
+        std::cout << "attack failed" << std::endl;
+    }
 
+    mpz_clears(p, q, g, y, x, h_m, r, s, k, x_found, NULL);
     return 0;
 }
